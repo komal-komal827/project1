f36c54377034d31a6e7019b87dd7cb540ee11a70
unknown <detakekomal572@gmail.com>
2025-06-22 15:57:20 +0530

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Demo from './forms_controll_component/Demo'
 
 //import'./index .css'
 
 
 
 createRoot(document.getElementById('root')).render(
- // <StrictMode>
-    
-  
- 
-  
+ //<StrictMode>
+  <Demo/>
 
- // </StrictMode>,
+ //</StrictMode>
 )
